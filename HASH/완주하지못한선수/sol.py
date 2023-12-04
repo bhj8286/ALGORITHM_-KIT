555b86374f5873b8e3b2d1b6f1139a1df03f77dd
bhj8286 <bjs219@naver.com>
2023-12-04 18:02:36 +0900

    LEVEL_UP

---
+++ HASH/완주하지못한선수/sol.py
@@ -0,0 +1,40 @@
+def solution(participant, completion):
+    answer = ''
+    for i in participant:
+        if participant.count(i) - 1 == completion.count(i):
+            answer = i
+
+    return answer
+
+
+print(solution(["leo", "kiki", "eden"], ["eden", "kiki"]))
+print(solution(["marina", "josipa", "nikola", "vinko", "filipa"], ["josipa", "filipa", "marina", "nikola"]))
+print(solution(["mislav", "stanko", "mislav", "ana"], ["stanko", "ana", "mislav"]))
+
+
+
+def solution(participant, completion):
+    hashdict = {}
+    sumhash1 = 0
+    sumhash2 = 0
+    answer = ''
+    for i in participant:
+        hashdict[hash(i)] = i
+        # print(hashdict)
+        sumhash1 += hash(i)
+    
+
+    for j in completion:
+        sumhash2 += hash(j)
+        
+    
+    answer = hashdict[sumhash1-sumhash2]
+
+
+
+
+    return answer
+
+print(solution(["leo", "kiki", "eden"], ["eden", "kiki"]))
+print(solution(["marina", "josipa", "nikola", "vinko", "filipa"], ["josipa", "filipa", "marina", "nikola"]))
+print(solution(["mislav", "stanko", "mislav", "ana"], ["stanko", "ana", "mislav"]))
---
+++ 스택/같은 숫자는 싫어/sol.py
@@ -0,0 +1,8 @@
+def solution(arr):
+    answer = []
+    # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    print('Hello Python')
+    return answer
+
+print(solution([1,1,3,3,0,1,1]))
+print(solution([4,4,4,3,3]))
