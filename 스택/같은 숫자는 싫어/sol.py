9b46884f77f75c6f087099ea7081eaaf7583e1d7
bhj8286 <bjs219@naver.com>
2023-12-04 18:12:35 +0900

    LEVEL_UP

--- 스택/같은 숫자는 싫어/sol.py
+++ 스택/같은 숫자는 싫어/sol.py
@@ -1,8 +1,24 @@
+# 내 풀이
 def solution(arr):
     answer = []
-    # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-    print('Hello Python')
+    for  i in arr:
+        if len(answer) == 0:
+            answer.append(i)
+        elif len(answer) != 0:
+            if answer[-1] != i:
+                answer.append(i)
+            else:
+                continue
     return answer
 
 print(solution([1,1,3,3,0,1,1]))
 print(solution([4,4,4,3,3]))
+
+
+# 다른 사람 비슷한 코드
+def no_continuous(s):
+    a = []
+    for i in s:
+        if a[-1:] == [i]: continue # a[-1:]은 마지막 원소를 꺼내 리스트로 만들어 준 거라 i에 리스트를 씌워야한다
+        a.append(i)
+    return a
