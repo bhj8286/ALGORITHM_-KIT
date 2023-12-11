3e67c5eff84fa6463a1bb3778e7c724d642f269d
bhj8286 <bjs219@naver.com>
2023-12-11 17:38:54 +0900

    study

--- 스택/프로세스/sol.py
+++ 스택/프로세스/sol.py
@@ -1,29 +1,31 @@
+# 시간 복잡도 문제 있음
 from string import ascii_uppercase
+from collections import deque
+
 def solution(priorities, location):
     answer = 0
     Alpha_list = list(ascii_uppercase)
-    AL_list = []
-    for i in range(len(priorities)):
-        AL_list.append(Alpha_list[i])
+    AL_list = deque([Alpha_list[i] for i in range(len(priorities))])
     check = dict(zip(AL_list, priorities))
-    print(AL_list)
     my = AL_list[location]
+    my_priorities = deque(priorities)
     final = []
-    while True:
-        for j in AL_list:
-            if check[j] == max(priorities):
-                final.append(j)
-                priorities.pop(0)
-            elif check[j] != max(priorities):
-                a = priorities.pop(0)
-                priorities.append(a)
-        answer = final.index(my)
+
+    while my_priorities:
+        max_priority = max(my_priorities)
+        if len(priorities) == 0:
+            break
+
+        while check[AL_list[0]] != max_priority:
+            AL_list.append(AL_list.popleft())
+            my_priorities.append(my_priorities.popleft())
+
+        final.append(AL_list.popleft())
+        my_priorities.popleft()
+
+    answer = final.index(my) + 1
 
     return answer
 
 print(solution([2, 1, 3, 2], 2))
 print(solution([1, 1, 9, 1, 1, 1], 0))
-
-
-
-
