733e2b623af41218370053895a5bed29f68ce0aa
bhj8286 <bjs219@naver.com>
2023-12-17 16:16:48 +0900

    solve

--- HASH/완주하지못한선수/sol.py
+++ HASH/완주하지못한선수/sol.py
@@ -23,18 +23,22 @@ def solution(participant, completion):
         # print(hashdict)
         sumhash1 += hash(i)
     
-
     for j in completion:
         sumhash2 += hash(j)
-        
     
     answer = hashdict[sumhash1-sumhash2]
-
-
-
-
+    
     return answer
 
 print(solution(["leo", "kiki", "eden"], ["eden", "kiki"]))
 print(solution(["marina", "josipa", "nikola", "vinko", "filipa"], ["josipa", "filipa", "marina", "nikola"]))
 print(solution(["mislav", "stanko", "mislav", "ana"], ["stanko", "ana", "mislav"]))
+
+
+# 다른 사람 풀이
+import collections
+
+
+def solution(participant, completion):
+    answer = collections.Counter(participant) - collections.Counter(completion)
+    return list(answer.keys())[0]
