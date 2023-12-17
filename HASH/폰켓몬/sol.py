09652d1dee7e5442cbdd0f7cc6ca99aac8470b1b
bhj8286 <bjs219@naver.com>
2023-12-17 16:32:05 +0900

    solve

---
+++ HASH/폰켓몬/sol.py
@@ -0,0 +1,13 @@
+def solution(nums):
+    answer = 0
+    max_pick = len(nums)/2 # 최대로 데려갈 수 있는 포켓몬의 수
+    nums = set(nums)
+    if len(nums) >= max_pick:  # 포켓몬의 종류가 최대로 데려갈 수 있는 포켓몬의 수보다 크거나 같은 경우는 
+        answer = max_pick      # 최대로 데려갈 수 있는 포켓몬의 수 = max_pick
+    else:                      #  포켓몬의 종류가 최대로 데려갈 수 있는 포켓몬의 수보다 적은 경우
+        answer = int(len(nums))  # 종류수만큼만 가져갈 수 있다
+    return answer
+
+print(solution([3,1,2,3]))
+print(solution([3,3,3,2,2,4]))
+print(solution([3,3,3,2,2,2]))
