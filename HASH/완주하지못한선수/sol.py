e7e55543ae6998bb935efe549a4bbcacb878432d
bhj8286 <bjs219@naver.com>
2024-01-08 20:03:24 +0900

    update

--- HASH/완주하지못한선수/sol.py
+++ HASH/완주하지못한선수/sol.py
@@ -38,7 +38,6 @@ print(solution(["mislav", "stanko", "mislav", "ana"], ["stanko", "ana", "mislav"
 # 다른 사람 풀이
 import collections
 
-
 def solution(participant, completion):
     answer = collections.Counter(participant) - collections.Counter(completion)
     return list(answer.keys())[0]
