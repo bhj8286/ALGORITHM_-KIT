b1eb318340dec9e04228f6235cd79d75b42048a2
bhj8286 <bjs219@naver.com>
2024-01-11 17:46:05 +0900

    update

--- HASH/전화번호 목록/sol.py
+++ HASH/전화번호 목록/sol.py
@@ -1,10 +1,59 @@
-from collections import Counter
+# 정확도: 70.8, 효율성꽝, tc2-3개 불통 
 def solution(phone_book):
     answer = True
-    
-            
+    for i in range(len(phone_book)-1):
+        for j in phone_book[i+1:len(phone_book)]:
+            if phone_book[i] == j[0:len(phone_book[i])]:
+                answer = False
+                break
     return answer
 
 print(solution(["119", "97674223", "1195524421"]))
 print(solution(["123","456","789"]))
 print(solution(["12","123","1235","567","88"]))
+
+# 수정본
+def solution(phone_book):
+    answer = True
+    phone_book.sort()
+    for i in range(len(phone_book)-1):
+        if phone_book[i] == phone_book[i+1][0:len(phone_book[i])]:
+            answer = False
+            break
+    return answer
+
+print(solution(["119", "97674223", "1195524421"]))
+print(solution(["123","456","789"]))
+print(solution(["12","123","1235","567","88"]))
+
+
+# 다른 사람 풀이
+def solution(phone_book):
+    answer = True
+    phone_book.sort()
+    for i in range(len(phone_book)-1):
+        if len(phone_book[i]) < len(phone_book[i+1]):  # --> 이거 왜 넣은 건지 모르겠음
+            if phone_book[i + 1][:len(phone_book[i])] == phone_book[i]:
+                answer = False
+                break
+    return answer
+
+# 해시 정석 풀이
+def solution(phone_book): 
+
+    # 1.Hash map생성
+    hash_map = {} 
+    for nums in phone_book: 
+        hash_map[nums] = 1 
+    
+    # 2.접두어가 Hash map에 존재하는지 찾기 
+    for nums in phone_book: 
+        arr = "" 
+        for num in nums: 
+            arr += num
+    
+            # 3. 본인 자체일 경우는 제외
+            if arr in hash_map and arr != nums:       
+                return False 
+                
+    return True
