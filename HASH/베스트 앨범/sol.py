6d4e3468d056d6d47848fef63e7be1604034765b
bhj8286 <bjs219@naver.com>
2024-01-12 17:38:34 +0900

    update

--- HASH/베스트 앨범/sol.py
+++ HASH/베스트 앨범/sol.py
@@ -1,6 +1,11 @@
 def solution(genres, plays):
     answer = []
-    
+    matching = dict(zip(plays,genres))
+    # print(matching)
+    plays.sort(reverse=True)
+    # print(plays)
+    for i in plays:
+        pass
     return answer
 
 print(solution(["classic", "pop", "classic", "classic", "pop"], [500, 600, 150, 800, 2500]))
