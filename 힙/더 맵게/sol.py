d519da4f383b2f5242d02a6b3fe9c3d44eea40d8
bhj8286 <bjs219@naver.com>
2024-01-16 21:37:14 +0900

    update

--- 힙/더 맵게/sol.py
+++ 힙/더 맵게/sol.py
@@ -12,3 +12,20 @@ def solution(scoville, K):
     return answer
 
 print(solution([1, 2, 3, 9, 10, 12], 7))
+
+import heapq
+def solution(scoville, K):
+    answer = 0
+    heapq.heapify(scoville)
+    while scoville[0] < K: ## scoville을 heapq리스트이므로 while min(scoville) < K:으로 시간낭비를 할 필요가 없다. 힙큐는 정렬이 되어 나오기 때문에
+        if len(scoville) == 1: ## 모든 음식을 섞어서 스코빌 지수 k이상으로 만들지 못한 경우는 if문에 걸린다.
+            return -1
+        mix_scovile = heapq.heappop(scoville) + (heapq.heappop(scoville)*2)
+        answer += 1
+        heapq.heappush(scoville,mix_scovile)
+    return answer
+
+print(solution([1, 2, 3, 9, 10, 12], 7))
+
+
+
