944c7f63f11b028945187725527f401d187d80db
bhj8286 <bjs219@naver.com>
2024-01-23 16:36:06 +0900

    update

--- 완전탐색/최소직사각형/sol.py
+++ 완전탐색/최소직사각형/sol.py
@@ -15,3 +15,7 @@ def solution(sizes):
 print(solution([[60, 50], [30, 70], [60, 30], [80, 40]]))
 print(solution([[10, 7], [12, 3], [8, 15], [14, 7], [5, 15]]))
 print(solution([[14, 4], [19, 6], [6, 16], [18, 7], [7, 11]]))
+
+# 다른 사람 풀이: 오...
+def solution(sizes):
+    return max(max(x) for x in sizes) * max(min(x) for x in sizes)
