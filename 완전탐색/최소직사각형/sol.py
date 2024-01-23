2106c40e63504f7c936dce754c6eebf10dc934e1
bhj8286 <bjs219@naver.com>
2024-01-23 16:32:36 +0900

    update

--- 완전탐색/최소직사각형/sol.py
+++ 완전탐색/최소직사각형/sol.py
@@ -1,10 +1,15 @@
 def solution(sizes):
     answer = 0
     re_sizes = []
+    garo = []
+    sero = []
     for i in sizes:
-        i.sort
-        re_sizes.append(i)
-    print(re_sizes)
+        re_sizes.append(sorted(i))
+    for j in range(len(re_sizes)):
+        garo.append(re_sizes[j][0])
+    for j in range(len(re_sizes)):
+        sero.append(re_sizes[j][1])
+    answer = max(garo)*max(sero)
     return answer
 
 print(solution([[60, 50], [30, 70], [60, 30], [80, 40]]))
